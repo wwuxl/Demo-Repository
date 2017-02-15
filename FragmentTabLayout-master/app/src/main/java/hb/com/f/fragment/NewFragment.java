01729352c4c2e816a8f513337f969ecd143e3840
wxl <619663954@qq.com>
2017-02-15 11:47:19 +0800

    frst commit

---
+++ FragmentTabLayout-master/app/src/main/java/hb/com/f/fragment/NewFragment.java
@@ -0,0 +1,31 @@
+package hb.com.f.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+
+public class NewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView=new TextView(getContext());
+        textView.setText("新闻页面");
+        textView.setTextSize(30);
+        textView.setTextColor(Color.BLUE);
+
+        Log.d("ActionFragment", "ActionFragment");
+
+        return textView;
+    }
+}
